96c5a0da100676e18be977fc0aab8c7fa8e6dd21
Nakib Momin <nbmomin14@gmail.com>
2015-03-17 23:16:42 -0400

    Added slider and action to get value of each color

--- Color Maker/ViewController.swift
+++ Color Maker/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +25,12 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func changeColorValue(sender: UISlider) {
+        var redValue = redSlider.value
+        var greenValue = greenSlider.value
+        var blueValue = blueSlider.value
+        
+        
+    }
 }
 
